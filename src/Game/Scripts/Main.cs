6745087601661388bb161a618a8fa9ef58bc2c32
syf <2654525303@qq.com>
2026-04-19 10:07:16 +0800

    adjust dial size on mouse button input

--- src/Game/Scripts/Main.cs
+++ src/Game/Scripts/Main.cs
@@ -1,9 +1,28 @@
 ﻿namespace AnalogClock;
 
-public partial class Main : Node2D
+public partial class Main : Node
 {
+    Vector2I _originalWindowSize;
+
     public override void _Ready()
     {
+        _originalWindowSize = GetWindow().Size;
         GD.Print("Hello from pixel art template!");
+    }
+
+    public override void _Input(InputEvent @event)
+    {
+        if (@event is InputEventMouseButton inputEventMouseButton)
+        {
+            const int windowSizeUpdateStep = 10;
+            var windowSizeIncrement = inputEventMouseButton.ButtonIndex switch
+            {
+                MouseButton.WheelUp => new Vector2I(windowSizeUpdateStep, windowSizeUpdateStep),
+                MouseButton.WheelDown => - new Vector2I(windowSizeUpdateStep, windowSizeUpdateStep),
+                MouseButton.Middle => _originalWindowSize - GetWindow().Size,
+                _ => Vector2I.Zero,
+            };
+            GetWindow().Size += windowSizeIncrement;
+        }
     }
 }
